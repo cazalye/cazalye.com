6db06276757bc07bc860386e9aeed130a31015ec
Emma <e.cazaly7@gmail.com>
2019-11-04 09:36:34 +0200

    Add landing page tsx and scss

---
+++ src/components/landingPage/landingPage.tsx
@@ -0,0 +1,18 @@
+// Code the Landing Page
+
+import "./landingPage.scss";
+
+import React, { Component } from 'react';
+import { string } from "prop-types";
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div className="hello">
+                <p>Hello World!</p>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
