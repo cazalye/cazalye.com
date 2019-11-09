17225071f49a99406d4536f2c70d64df583705b8
Emma <e.cazaly7@gmail.com>
2019-11-09 16:08:29 +0200

    Add blog component with data from WP API

--- src/components/blog/blog.tsx
+++ src/components/blog/blog.tsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import { getTravelPosts } from '../../API/posts';
+import "./blog.scss";
 
-class Blog extends Component {
+class Blog extends Component<any, any> {
+    travelPosts: any;
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            posts: getTravelPosts()
+        };
+
+
+    }
     render() {
+        let postsContent=[];
+        for (const post of this.state.posts) {
+
+
+            postsContent.push(
+                <div className="post-container">
+                    <h1>{post.title.rendered}</h1>
+                    <h4>{post.date}</h4>
+                </div>
+            );
+        }
         return (
-            <div/>
+            <div id="blogs-page">
+                <h1>Blog Posts</h1>
+                <div className="posts-container">
+                    {postsContent}
+                </div>
+            </div>
         );
     }
 }
--- src/components/view.tsx
+++ src/components/view.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import LandingPage from './landingPage/landingPage';
 import {Switch, Route} from 'react-router-dom';
+import Blog from './blog/blog';
 
 
 const View = () => (
     <Switch>
         <Route exact={true} path="/" component={LandingPage}/>
+        <Route exact={true} path='/blog' component={Blog}/>
     </Switch>
 );
 
