e1f34090f4bb417e2a3894425d9983be2b1d72e1
Emma <e.cazaly7@gmail.com>
2019-11-12 16:33:36 +0200

    Code h tags in app.scss instead of individual files

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import LandingPage from './landingPage/landingPage';
 import {Switch, Route} from 'react-router-dom';
 import Blog from './blog/blogList';
+import About from './about/about';
 
 
 const View = () => (
     <Switch>
         <Route exact={true} path="/" component={LandingPage}/>
         <Route exact={true} path='/blog' component={Blog}/>
+        <Route exact={true} path='/about' component={About}/>
     </Switch>
 );
 
