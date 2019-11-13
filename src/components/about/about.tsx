c49b4f8173c6aa5e007f7390500c50335a8493d5
Emma <e.cazaly7@gmail.com>
2019-11-13 17:23:26 +0200

    Add content and style about page

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -11,21 +11,10 @@ class About extends Component {
                         <h3>at home and abroad</h3>
                     </div>
                 <div className="about-content">
-                    <p> I have no idea what to write here. I hate everything I've tried</p>
-                    {/* <p> <strong>Explore:</strong> Adventures can be found everywhere- thousands of miles away on the other side of the world, or right outside your front door in your own neighbourhood. All you need for an adventure is a sense of wonder. </p>
-                    <p> <strong>Create:</strong> Exploring the world arounnd us provides infinite inspiration for creativity...something about seeing mine in the photos and postcards etc</p>
-                    <p> <strong>Discover:</strong> The thing I love most about travel is getting utterly immersed and 'lost' in the foreignness of a different culture and landscape. It gives me distance from everything fmiliar so I can see myself clearly.
-
-                     As Marcel Proust once said "The real voyage of discovery consists not in seeking new lands but in seeing with new eyes"
-               </p> */}
-               </div>
-
-                    {/* In the age of Instagram, Youtube and Google living in your pocket, there's no shortage of images and articles proclaiming the latest hot travel destination. On top of this, traveling the world has never been more accesible. This can make it extremely overwhelming to decide explore next.
-
-                    As a lover of nature and photography, the number one way I choose my travel destinations is by beautiful photos.
-                    And you don't need to fly thousands of miles to have an adventure. */}
+                    <p> When it comes to researching future adventures, there's no shortage of detailed articles on destinations, activities and Instagram-worthy locations. With Google living in your pocket, the choices and information load can often feel overwhelming. <hr/> With this in mind, I have chosen to limit the detail of information on this site. You won't find comprehensive itineraries or long-form posts that take hours to trawl in search of key points or visually pleasing images. You can find all that elsewhere. <hr/> Instead, you will find carefully curated posts taking one of two formats: <br/> <strong>Blog Posts</strong> in list-format, describing in under 500 words (less than 5 minute read) the best 5-10 activities, sites or coffee spots in a certain destination<br/> And <strong>Photo Diaries</strong>, which take you on a visual journey through different countries
+                    </p>
+                </div>
             </div>
-
         );
     }
 }
