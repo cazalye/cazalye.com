d6503e3dfb94769532abb6a0d6bd2239c2ebdc78
Emma <e.cazaly7@gmail.com>
2019-11-13 17:25:22 +0200

    Add text to photo diaries page

--- src/components/photoDiaries/photoDiaries.tsx
+++ src/components/photoDiaries/photoDiaries.tsx
@@ -9,3 +9,6 @@ class PhotoDiaries extends Component {
 }
 
 export default PhotoDiaries;
+
+
+// Add to analogue diary into:  One of my favourite things about travelling to and exploring new places is to wander aimlessly with my camera in hand, capturing local street life, landscapes and foreign feelings. As an introvert, it also gives me some time to myself, to escape the busy and people-filled aspects of travel. As the photographer Alec Soth wonderfully describes it; "I fell in love with photography because it was an excuse to wander around alone"..... Using a film camera heightens the sense of being lost in another culture- you're forced to slow down and really think about the image composition as there's a limited number of (rather expensive) images to shoot.
