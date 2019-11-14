59c0638bdde0aa4f4071af2c8ba07553240cf95a
Emma <e.cazaly7@gmail.com>
2019-11-14 18:02:51 +0200

    Add 'start here' to landing page, links to about

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -10,11 +10,13 @@ class LandingPage extends Component {
                         <h2>Inspiration for your adventures</h2>
                         <h3>at home and abroad</h3>
                     </div>
+                    <div className="start-here">
+                        <p>Start Here</p>
+                    </div>
                     <div className="coffee">
                         <h3>The Best Coffee & Brunch <br/> Spots Around the World</h3>
                         <hr/>
-                        <h3> Travel Budgets</h3>
-                        <hr/>
+                        {/* <h3> Travel Budgets</h3> */}
                     </div>
                 </div>
             </div>
