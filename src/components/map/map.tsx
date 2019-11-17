34c4f7d420e8015dd7b4db90a48882d91dd2a335
Emma <e.cazaly7@gmail.com>
2019-11-17 12:43:12 +0200

    Install React-map-gl and create Map component

---
+++ src/components/map/map.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import {} from 'react-map-gl';
+
+class Map extends Component {
+    render() {
+        return (
+            <div>
+                <p>hello hello</p>
+            </div>
+        );
+    }
+}
+
+export default Map;
