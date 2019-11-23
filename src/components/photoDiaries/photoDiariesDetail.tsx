f6fc7e49051777e4c63cf81680e8f8f54909c5d2
Filippo Zonta <zontafil@gmail.com>
2019-11-23 20:20:52 +0200

    Fix photoDiary detail not working with new APIs

--- src/components/photoDiaries/photoDiariesDetail.tsx
+++ src/components/photoDiaries/photoDiariesDetail.tsx
@@ -5,6 +5,13 @@ import NavbarHider from '../navbar-hider/NavbarHider';
 import NotFound from '../404/404';
 import {Link} from "react-router-dom";
 
+interface PhotoDiariesDetailState {
+    photoDiary: Post | null;
+    error: boolean;
+    relatedPhotoDiaries: Post[];
+    page: number;
+}
+
 /**
  * Randomize array element order in-place.
  * Using Durstenfeld shuffle algorithm.
@@ -18,12 +25,11 @@ function shuffleArray(array: any) {
     }
 }
 
-
-class PhotoDiariesDetail extends Component<any, any> {
+class PhotoDiariesDetail extends Component<any, PhotoDiariesDetailState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            "photoDiary": {},
+            "photoDiary": null,
             error: false,
             relatedPhotoDiaries: [],
             page: 0
@@ -71,97 +77,102 @@ class PhotoDiariesDetail extends Component<any, any> {
 
 
     renderSection() {
-        if (this.state.page === 0) {
-            return (
-                <div className="cover-page">
-                    <div className="cover-photo" style={{backgroundImage: `url(${this.state.photoDiary.feature_image_url})`}}>
-                        <h1>{this.state.photoDiary.title}</h1>
-                    </div>
-                </div>
-            );
-        }
-        else if  (this.state.page === 1){
-            return (
-                <div className="double-page-spread">
-                    <div className="left-page-key-details book-page">
-                        <div className="photo-inner-shadow"/>
-                        <div className="photo-outer-shadow"/>
-                        <div className="photo-page-container">
-                            <h3>Location<br/>Date<br/>Camera<br/>Lens<br/></h3>
+        if (!this.state.photoDiary) {
+            return (null);
+        } else {
+            if (this.state.page === 0) {
+                // debugger
+                return (
+                    <div className="cover-page">
+                        <div className="cover-photo" style={{backgroundImage: `url(${this.state.photoDiary.images[0].sizes.large})`}}>
+                            <h1>{this.state.photoDiary.title}</h1>
                         </div>
                     </div>
-                    <div className="right-page-summary book-page">
-                        <div className="photo-page-container">
-                            <p>Summary of the place/photo diary</p>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        // CHANGE THIS conditional statement to rely on flag of PORTRAIT / LANSCAPE which come from new API
-        else if (this.state.page === 2){
-            return (
-                <div className="double-page-spread">
-                    <div className="left-page-key-details book-page">
-                        <div className="photo-inner-shadow"/>
-                        <div className="photo-outer-shadow"/>
-                        <div className="photo-page-container">
-                            <div className="photo" style={{backgroundImage: `url(${this.state.photoDiary.images[0]})`}}/>
-                        </div>
-                    </div>
-                    <div className="right-page-summary book-page">
-                        <div className="photo-page-container">
-                            <div className="photo" style={{backgroundImage: `url(${this.state.photoDiary.images[1]})`}}/>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        // CHANGE THIS conditional statement to rely on flag of PORTRAIT / LANSCAPE which come from new API
-        else if (this.state.page === 3) {
-            return (
-                <div className="double-page-spread landscape">
-                    <div className="left-page-key-details book-page">
-                        <div className="photo-inner-shadow"/>
-                        <div className="photo-outer-shadow"/>
-                        <div className="photo-page-container">
-                            <div className="photo landscape" style={{backgroundImage: `url(${this.state.photoDiary.images[2]})`}}/>
-                        </div>
-                    </div>
-                    <div className="right-page-summary book-page">
-                        <div className="photo-page-container"/>
-                    </div>
-                </div>
-            );
-        }
-        else if (this.state.page === 4) {
-            let relatedPosthtml: any = [];
-            if (this.state.relatedPhotoDiaries.length > 1) {
-                relatedPosthtml = [
-                    (
-                            <Link className="photo-diary-preview" to={"/photoDiaries/" + this.state.relatedPhotoDiaries[0].slug} style={{backgroundImage: `url(${this.state.relatedPhotoDiaries[0].featureImageSizes.large})`}}>
-                                <h2>{this.state.relatedPhotoDiaries[0].title}</h2>
-                            </Link>
-                    ),
-                    (
-                        <Link className="photo-diary-preview" to={"/photoDiaries/" + this.state.relatedPhotoDiaries[1].slug} style={{backgroundImage: `url(${this.state.relatedPhotoDiaries[1].featureImageSizes.large})`}}>
-                            <h2>{this.state.relatedPhotoDiaries[1].title}</h2>
-                        </Link>
-                    )
-                ];
+                );
             }
-            return (
-                <div className="double-page-spread back-page-container">
-                    <div className="left-page-key-details book-page">
-                        <div className="photo-page-container">
-                            <div className="back-page">
-                                <h1>If you liked this photo diary, you might also like...</h1>
-                                {relatedPosthtml}
+            else if  (this.state.page === 1){
+                return (
+                    <div className="double-page-spread">
+                        <div className="left-page-key-details book-page">
+                            <div className="photo-inner-shadow"/>
+                            <div className="photo-outer-shadow"/>
+                            <div className="photo-page-container">
+                                <h3>Location<br/>Date<br/>Camera<br/>Lens<br/></h3>
+                            </div>
+                        </div>
+                        <div className="right-page-summary book-page">
+                            <div className="photo-page-container">
+                                <p>Summary of the place/photo diary</p>
                             </div>
                         </div>
                     </div>
-                </div>
-            );
+                );
+            }
+            // CHANGE THIS conditional statement to rely on flag of PORTRAIT / LANSCAPE which come from new API
+            else if (this.state.page === 2){
+                return (
+                    <div className="double-page-spread">
+                        <div className="left-page-key-details book-page">
+                            <div className="photo-inner-shadow"/>
+                            <div className="photo-outer-shadow"/>
+                            <div className="photo-page-container">
+                                <div className="photo" style={{backgroundImage: `url(${this.state.photoDiary.images[0].sizes.large})`}}/>
+                            </div>
+                        </div>
+                        <div className="right-page-summary book-page">
+                            <div className="photo-page-container">
+                                <div className="photo" style={{backgroundImage: `url(${this.state.photoDiary.images[1].sizes.large})`}}/>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+            // CHANGE THIS conditional statement to rely on flag of PORTRAIT / LANSCAPE which come from new API
+            else if (this.state.page === 3) {
+                return (
+                    <div className="double-page-spread landscape">
+                        <div className="left-page-key-details book-page">
+                            <div className="photo-inner-shadow"/>
+                            <div className="photo-outer-shadow"/>
+                            <div className="photo-page-container">
+                                <div className="photo landscape" style={{backgroundImage: `url(${this.state.photoDiary.images[2]})`}}/>
+                            </div>
+                        </div>
+                        <div className="right-page-summary book-page">
+                            <div className="photo-page-container"/>
+                        </div>
+                    </div>
+                );
+            }
+            else if (this.state.page === 4) {
+                let relatedPosthtml: any = [];
+                if (this.state.relatedPhotoDiaries.length > 1) {
+                    relatedPosthtml = [
+                        (
+                                <Link className="photo-diary-preview" to={"/photoDiaries/" + this.state.relatedPhotoDiaries[0].slug} style={{backgroundImage: `url(${this.state.relatedPhotoDiaries[0].images[0].sizes.large})`}}>
+                                    <h2>{this.state.relatedPhotoDiaries[0].title}</h2>
+                                </Link>
+                        ),
+                        (
+                            <Link className="photo-diary-preview" to={"/photoDiaries/" + this.state.relatedPhotoDiaries[1].slug} style={{backgroundImage: `url(${this.state.relatedPhotoDiaries[1].images[0].sizes.large})`}}>
+                                <h2>{this.state.relatedPhotoDiaries[1].title}</h2>
+                            </Link>
+                        )
+                    ];
+                }
+                return (
+                    <div className="double-page-spread back-page-container">
+                        <div className="left-page-key-details book-page">
+                            <div className="photo-page-container">
+                                <div className="back-page">
+                                    <h1>If you liked this photo diary, you might also like...</h1>
+                                    {relatedPosthtml}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
         }
     }
     incPage(increment: number) {
