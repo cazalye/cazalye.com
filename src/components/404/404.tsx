b94895ded9494686615faabd75d332c13320221d
Emma <e.cazaly7@gmail.com>
2019-11-23 18:38:52 +0200

    Add 404 error page

---
+++ src/components/404/404.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./404.scss";
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div className="not-found-component">
+                <h1>404 - Page not found</h1>
+            </div>
+        );
+    }
+}
+
+export default NotFound;
