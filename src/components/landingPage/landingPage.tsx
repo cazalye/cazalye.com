48cf8ada457ec0885728504d3d1a6b77c53796a5
Filippo Zonta <zontafil@gmail.com>
2019-11-24 09:44:18 +0200

    Add slideshow in landing page

--- src/components/landingPage/landingPage.tsx
+++ src/components/landingPage/landingPage.tsx
@@ -1,11 +1,67 @@
 import "./landingPage.scss";
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import {Link} from "react-router-dom";
 import Map from "../map/map";
 import Footer from "../footer/footer";
-import Navbar from "../navbar/navbar";
+import {getPhotoDiaries, Post} from "../../API/posts";
 
-class LandingPage extends Component {
+interface LandingPageState {
+    // page: number | null;
+    page: number;
+    initialized: boolean;
+    photoDiaries: Post[];
+}
+
+class LandingPage extends Component<any, LandingPageState> {
+    numImages = 3;
+    state: LandingPageState = {
+        page: 0,
+        initialized: false,
+        photoDiaries: []
+    };
+    async componentDidMount() {
+        const photoDiaries = await getPhotoDiaries({
+            limit: this.numImages
+        });
+
+        this.setState({
+            initialized: true,
+            photoDiaries: photoDiaries
+        });
+        setInterval(() => {
+            this.setState({
+                page: this.state.page > this.numImages - 2 ? 0 : this.state.page + 1
+            });
+        }, 5000);
+    }
     render() {
+        let titleHtml = (null);
+        let imagesLayerContainer = (null);
+        if (this.state.initialized) {
+            const activePhotoDiary = this.state.photoDiaries[this.state.page];
+            titleHtml = (<h3>{this.state.photoDiaries[this.state.page].title}</h3>);
+
+            const imageLayers = [];
+            for (const photoDiary of this.state.photoDiaries) {
+                const style = {
+                    backgroundImage: `url(${photoDiary.featureMedia.sizes.large})`
+                };
+                const activeClass = photoDiary.id === activePhotoDiary.id ? "active-layer" : "inactive-layer";
+                imageLayers.push(
+                    <div className={`image-layer ${activeClass}`} style={style}>
+                        <Link className="photo-diary-title" to={`/photoDiaries/${photoDiary.slug}`}>
+                            <h3>{photoDiary.title}</h3>
+                        </Link>
+                    </div>
+                );
+            }
+
+            imagesLayerContainer = (
+                <div className="image-layers-container">
+                    {imageLayers}
+                </div>
+            );
+        }
         return (
             <div id="landing">
                 <div id="landing1">
@@ -16,11 +72,7 @@ class LandingPage extends Component {
                     <div className="start-here">
                         <p>Start Here</p>
                     </div>
-                    <div className="coffee">
-                        <h3>The Best Coffee & Brunch <br/> Spots Around the World</h3>
-                        <hr/>
-                        {/* <h3> Travel Budgets</h3> */}
-                    </div>
+                    {imagesLayerContainer}
                 </div>
                 <div className="landing-section">
                     <Map/>
