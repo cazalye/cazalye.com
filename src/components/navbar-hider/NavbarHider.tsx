5e8a03ad8b1af4aa1b0536ef30cde083948b1646
Emma <e.cazaly7@gmail.com>
2019-12-02 09:07:12 +1100

    Change accent color to green

--- src/components/navbar-hider/NavbarHider.tsx
+++ src/components/navbar-hider/NavbarHider.tsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 type NavbarHiderProps = {
     hamburgerMode: boolean;
-    blueTitle: boolean;
+    greenTitle: boolean;
 };
 
 class NavbarHider extends Component<NavbarHiderProps> {
     static defaultProps = {
         hamburgerMode: false,
-        blueTitle: false
+        greenTitle: false
     };
     componentDidMount() {
         document.body.classList.toggle('hamburger-mode', this.props.hamburgerMode);
-        document.body.classList.toggle('blue-title', this.props.blueTitle);
+        document.body.classList.toggle('green-title', this.props.greenTitle);
     }
     componentWillUnmount() {
         document.body.classList.remove('hamburger-mode');
-        document.body.classList.remove('blue-title');
+        document.body.classList.remove('green-title');
     }
     render() {
         return null;
