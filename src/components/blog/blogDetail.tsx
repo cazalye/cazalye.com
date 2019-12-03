d8459358538ecba2f1cf9a1d1a0f6401601377b8
Emma <e.cazaly7@gmail.com>
2019-12-03 15:30:38 +1100

    Create blog detail files

---
+++ src/components/blog/blogDetail.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { getBlogPosts, Post } from '../../API/posts';
+import "./blogDetail.scss";
+import { Link } from 'react-router-dom';
+
+class blogDetail extends Component<any {
+
+
+    render() {
+        const getBlogPosts
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
