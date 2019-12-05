5f90c5b5ff80111c6dec7a144faf89c7b74a2b75
Emma <e.cazaly7@gmail.com>
2019-12-05 17:12:37 +1100

    Style PD list

--- src/components/photoDiaries/photoDiariesList.tsx
+++ src/components/photoDiaries/photoDiariesList.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import PostsSlideshow from "../postsSlideshow/postsSlideshow";
 import "./photoDiariesList.scss";
+import NavbarHider from '../navbar-hider/NavbarHider';
+
 
 class PhotoDiariesList extends Component {
     render() {
         return (
             <div>
+                <NavbarHider hamburgerMode={false} greenTitle={false} hideTitle={false}/>
                 <PostsSlideshow/>
             </div>
         );
