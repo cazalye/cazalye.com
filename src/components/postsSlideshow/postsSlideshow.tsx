298840ecb8e548aef26598b534599d2e8bab4a8e
Emma <e.cazaly7@gmail.com>
2019-12-16 11:28:08 +1100

    Add & style numbers on photo diary slider

--- src/components/postsSlideshow/postsSlideshow.tsx
+++ src/components/postsSlideshow/postsSlideshow.tsx
@@ -15,6 +15,7 @@ interface PostsSlideshowProps {
 
 class PostsSlideshow extends Component<PostsSlideshowProps, PostsSlideshowState> {
     numImages = 3;
+    interval: any;
     state: PostsSlideshowState = {
         page: 0,
         initialized: false,
@@ -41,11 +42,26 @@ class PostsSlideshow extends Component<PostsSlideshowProps, PostsSlideshowState>
             initialized: true,
             posts: posts
         });
-        setInterval(() => {
+
+        this.resetInterval();
+    }
+
+    resetInterval() {
+        if (this.interval) {
+            clearInterval(this.interval);
+        }
+        this.interval = setInterval(() => {
             this.setState({
                 page: this.state.page > this.numImages - 2 ? 0 : this.state.page + 1
             });
         }, 5000);
+    }
+
+    slide(page: number) {
+        this.setState({
+            page: page
+        });
+        this.resetInterval();
     }
 
     render() {
@@ -56,23 +72,40 @@ class PostsSlideshow extends Component<PostsSlideshowProps, PostsSlideshowState>
             titleHtml = (<h3 dangerouslySetInnerHTML={{__html: this.state.posts[this.state.page].title}}/>);
 
             for (const photoDiary of this.state.posts) {
+
+        
                 const style = {
                     backgroundImage: photoDiary.featureMedia ? `url(${photoDiary.featureMedia.sizes.large})`: ""
                 };
                 const activeClass = photoDiary.id === activePhotoDiary.id ? "active-layer" : "inactive-layer";
                 imageLayers.push(
                     <div className={`image-layer ${activeClass}`} style={style}>
-                        <Link className="photo-diary-title" to={`/photoDiaries/${photoDiary.slug}`}>
-                            <h3 dangerouslySetInnerHTML={{__html: photoDiary.title}}/>
-                            <p>read the diary</p>
-                        </Link>
+                        <div className="photo-diary-text">
+                            <Link className="photo-diary-title" to={`/photoDiaries/${photoDiary.slug}`}>
+                                <h3 dangerouslySetInnerHTML={{__html: photoDiary.title}}/>
+                                <p>read the diary</p>
+                            </Link>
+                        </div>
                     </div>
                 );
             }
         }
+        // build the numbers lists
+        const postsNumbers: any = [];
+        for (let i=0; i < this.state.posts.length; i++) {
+            let className = "inactive number-container";
+            if (i === this.state.page) {
+                className = "active number-container";
+            }
+            postsNumbers.push(<div className={className} onClick={() => this.slide(i)}><h4>{i+1}</h4></div>);
+        }
         return (
             <div className="posts-slideshow">
                 {imageLayers}
+                <div className="post-numbers-text">
+                    <h1>Browse Photo Diaries</h1>
+                </div>
+                <div className="posts-numbers">{postsNumbers}</div>
             </div>
         );
     }
