afdce804f5fbc01b99efd31ea4a2897bf472dbed
Emma <e.cazaly7@gmail.com>
2019-12-26 15:23:36 +1100

    Start Stone Paper page

--- src/components/stonePaper/stonePaper.tsx
+++ src/components/stonePaper/stonePaper.tsx
@@ -2,14 +2,18 @@ import "./stonePaper.scss";
 import Footer from "../footer/footer";
 import NavbarHider from "../navbar-hider/NavbarHider";
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-
 
 class StonePaper extends Component {
     render() {
         return (
-            <div className="stone-paper-about">
-                <p>About Stone Paper</p>
+            <div id="stone-paper-about">
+                <NavbarHider transparentRowHide={false} hamburgerMode={false} greenTitle={true} hideTitle={false}/>
+                <div className="stone-paper-header">
+                    <h1>Eco-friendly Stone Paper Travel Postcards</h1>
+                    <h2>Beautiful, adventure-inspired travel photogrpahy printed on the most eco-friendly paper in existence</h2>
+                    <a className="etsy-link" href="https://www.etsy.com/au/shop/cazalye"> Visit my Etsy store to order your Stone Paper Travel Postcards now</a>
+                </div>
+                <Footer/>
             </div>
         );
     }
