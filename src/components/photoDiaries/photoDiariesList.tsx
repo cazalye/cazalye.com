31fd094c7ef2657b30fb68560a6ec68094295270
Emma <e.cazaly7@gmail.com>
2020-01-12 08:52:27 +1100

    Add actual api data to photo diary mosaic

--- src/components/photoDiaries/photoDiariesList.tsx
+++ src/components/photoDiaries/photoDiariesList.tsx
@@ -3,24 +3,39 @@ import {getPhotoDiaries, Post, getBlogPosts} from "../../API/posts";
 import PostsSlideshow from "../postsSlideshow/postsSlideshow";
 import "./photoDiariesList.scss";
 import NavbarHider from '../navbar-hider/NavbarHider';
+import {Link} from "react-router-dom";
 
-class PhotoDiariesList extends Component {
+class PhotoDiariesList extends Component<any, any> {
+    // initialise empty array of posts, to be run while the data is loading
+    state: any = {
+        posts: []
+    };
 
+    // retrieve data
+    async componentDidMount() {
+        const posts = await getPhotoDiaries();
+        this.setState({
+            posts: posts
+        });
+    }
+
+    // Build UI - create html array, loop through feature images and add them to the html object
     render() {
-        // for (const photoDiary of this.state.posts) {
-        //     const style = {
-        //         backgroundImage: photoDiary.featureMedia ? `url("${photoDiary.featureMedia.sizes.large}")`: ""
-        // };
-        // <div  className="photo-cover" style={style}>
-        //     <Link className="photo-cover-link" to={`/photoDiaries/${photoDiary.slug}`}/>
-        //     <div className="photo-diary-text">
-        //         <Link className="photo-diary-title" to={`/photoDiaries/${photoDiary.slug}`}>
-        //             <h3 dangerouslySetInnerHTML={{__html: photoDiary.title}}/>
-        //             <p>read the diary</p>
-        //         </Link>
-        //     </div>
-        // </div>
 
+        const photoDiariesHTML = [];
+        for (const photoDiary of this.state.posts) {
+            const style = {
+                backgroundImage: photoDiary.featureMedia ? `url("${photoDiary.featureMedia.sizes.large}")`: ""
+            };
+            photoDiariesHTML.push(
+                <Link className="photo-diary-title" to={`/photoDiaries/${photoDiary.slug}`}>
+                    <div className="cover-photo" style={style}>
+                    <h3 className="cover-title" dangerouslySetInnerHTML={{__html: photoDiary.title}}/>
+                    <p>read the diary</p>
+                    </div>
+                </Link>
+            );
+        };
 
 
         return (
@@ -31,13 +46,9 @@ class PhotoDiariesList extends Component {
                     <h1>Photo Diaries</h1>
                     <p>Summary of what the photo diaries are about <br/><br/><br/> Images to right are just examples. need to be linked to the api of photo diaries <br/><br/><br/> Also try to align to bottom and make some images go over multiple columns so that sizes vary more <br/><br/><br/> Play around with Navbar and text colours</p>
                 </div>
-                <div className="photo-mosaic">
-                    <img src="img/background_sepia_cropped.jpg" alt="beach in sepia tones"/>
-                    <img src="img/rafting.jpg" alt="rafting montenegro"/>
-                    <img src="img/1.jpg" alt="test 1"/>
-                    <img src="img/2.jpg" alt="test 2"/>
-                    <img src="img/3.jpg" alt="test 3"/>
-                </div>
+                    <div className="photo-mosaic">
+                        {photoDiariesHTML}
+                    </div>
             </div>
         );
     }
