222e6a6787e4eb093dd771777ac36ce9bd9840c6
Emma <e.cazaly7@gmail.com>
2020-01-18 15:31:10 +1100

    Begin to code onScroll for navbar opacity

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './styles/App.scss';
 import View from './components/view';
 import Navbar from './components/navbar/navbar';
 
 
-const App: React.FC = () => {
 
-  return (
-    <div className="App">
-        <Navbar/>
-        <View/>
-    </div>
-  );
+// const App: React.FC = () => {
+class App extends Component<any, any> {
+
+  pageScrolled() {
+    console.log("scrolling")
+  }
+
+  render() {
+    return (
+      <div onScroll={e => {this.pageScrolled();}} className="App">
+          <Navbar/>
+          <View/>
+      </div>
+    );
+
+  }
 };
 
 export default App;
