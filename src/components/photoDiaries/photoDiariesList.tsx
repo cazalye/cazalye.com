028039906f0160bc5b3a190604d1fb80332a48c5
Emma <e.cazaly7@gmail.com>
2020-02-02 22:11:57 +1100

    Make mosaic for photo diary list

--- src/components/photoDiaries/photoDiariesList.tsx
+++ src/components/photoDiaries/photoDiariesList.tsx
@@ -23,6 +23,66 @@ class PhotoDiariesList extends Component<any, any> {
 
     render() {
 
+        const photoDiariesHTML = [];
+        for (const photoDiary of this.state.posts) {
+
+            const isPortrait = Math.floor(Math.random() * 4 + 1)
+
+            if (isPortrait !== 1) {
+                const style = {
+                    backgroundImage: photoDiary.featureMedia ? `url("${photoDiary.featureMedia.sizes.large}")`: ""
+                };
+                photoDiariesHTML.push(
+                    <div className="masonry-layout-panel">
+                        <div className="masonry-layout-panel__content">
+                            {/* <img src={photoDiary.featureMedia.sizes.large}/> */}
+                            <div className="landscape-image-container" style={style}/>
+                        </div>
+                    </div>
+                );
+            } else {
+                for (const spread of photoDiary.spreads) {
+                    if (spread.aspectRatio === "portrait") {
+                        const style1 = {
+                            backgroundImage: spread.images.length ? `url("${spread.images[0].sizes.large}")`: ""
+                        };
+                        const style2 = {
+                            backgroundImage: spread.images.length > 1 ? `url("${spread.images[1].sizes.large}")`: ""
+                        };
+                        photoDiariesHTML.push(
+                            <div className="masonry-layout-panel masonry-layout-cluster masonry-layout-cluster--vertical">
+                                <div className="masonry-layout-cluster__segment masonry-layout-cluster__segment--column">
+                                    <div className="portrait-image-container masonry-layout-panel masonry-layout-cluster__segment" style={style1}>
+                                        <div className="masonry-layout-panel__content.bg--green.tx--white">
+                                            <div className="portrait-image-container" style={style1}/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="masonry-layout-cluster__segment masonry-layout-cluster__segment--column">
+                                    <div className="portrait-image-contrainer masonry-layout-panel masonry-layout-cluster__segment" style={style2}>
+                                        <div className="masonry-layout-panel__content.bg--green.tx--white">
+                                            <div className="portrait-image-container" style={style2}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
+                }
+            }
+        }
+
+        return (
+            <div id="photo-diary-list">
+                <div className="masonry-layout">
+                    {photoDiariesHTML}
+                </div>
+            </div>
+        );
+    }
+
+    render_old() {
+
         if (!this.state.posts.length) {
             return (
                 <div id="photo-diary-list">
