de56a43cd748c5a4fd8f686d1bde796c2c751ecd
Emma <e.cazaly7@gmail.com>
2020-02-22 09:33:46 +1100

    Tweak about me content and style

--- src/components/about/aboutMe.tsx
+++ src/components/about/aboutMe.tsx
@@ -18,9 +18,9 @@ class AboutMe extends Component{
                     </div>
                     <div className="about-me-content">
                         <p>Hi- I'm Emma Cazaly- an Australian with a love of travel and everything outdoors. <strong> Cazalye is a combination of my surname and first initial.</strong><hr/>
-                        My love of travel and outdoor adventures have led me to explore countless destinations around the world, yet I believe that adventure is not only about exploring new places but also a mindset of curiosity, playfullness and resilience. </p>
+                        My love of travel and outdoor adventures have led me to explore countless destinations around the world, yet I believe that adventure is not only about exploring new places but also about a mindset. One of curiosity, playfullness, excitement and resilience. Adventure, in otherwords, is found within. </p>
 
-                        <p className="para3"> I designed this travel blog to share my favourite photos and tips about places I've been- especially the things I wish I'd known before arriving in a new place. <hr/> <strong>Enjoy Exploring!</strong></p>
+                        <p className="para3"> I designed this travel blog to share my favourite photos and tips about places I've been- especially the things I wish I'd known before arriving in a new destination. I also hope that my images and words inspire you to seek out adventure everyday- whether that be in small moments in your day-to-day life, or on big adventures out in the world. <hr/> <strong>Enjoy Exploring!</strong></p>
                     </div>
                     <div className="quote">
                         <h3> <i>"The real voyage of discovery consists <br/>not in seeking new landscapes, <br/>but in having new eyes."</i></h3>
