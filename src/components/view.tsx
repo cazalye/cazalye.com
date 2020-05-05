1bd0d4b92dbeeefe5f7cfb435c6ddae4c01a6f74
Emma Cazalye <e.cazaly7@gmail.com>
2020-05-05 17:53:21 +1000

    begin to add portfolio pages to view

--- src/components/view.tsx
+++ src/components/view.tsx
@@ -13,6 +13,10 @@ import PhotoDiariesListPage from './photoDiaries/photoDiariesListPage';
 import SearchResults from './searchResults/searchResults';
 import Categories from './categories/categories';
 import Portfolio from './portfolio/portfolio';
+import PortfolioContact from './portfolio/portfolioContact';
+import PortfolioAbout from './portfolio/portfolioAbout';
+import PhotographyTravel from './portfolio/photographyTravel';
+// import Resume from './portfolio/resume';
 
 
 
@@ -30,6 +34,10 @@ const View = () => (
         <Route exact={true} path='/search/:searchQuery' component={SearchResults}/>
         <Route exact={true} path='/categories' component={Categories}/>
         <Route exact={true} path='/portfolio' component={Portfolio}/>
+        <Route exact={true} path='/contact' component={PortfolioContact}/>
+        <Route exact={true} path='/portfolioabout' component={PortfolioAbout}/>
+        <Route exact={true} path='/photography' component={PhotographyTravel}/>
+        {/* <Route exact={true} path='/resume' component={Resume}/> */}
     </Switch>
 );
 
